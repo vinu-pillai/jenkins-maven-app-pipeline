89934be42259ca0cb05c24b9ae0e8b98a20b18be
Ubuntu <ubuntu@ip-172-31-11-55.ap-south-1.compute.internal>
2021-05-18 14:01:51 +0000

    modified build.sh and added few dockerfiles

--- 01_build.sh
+++ 01_build.sh
@@ -2,11 +2,13 @@
 
 # Copy the new jar to the build location
 echo "******* COPYING THE JAR FILE ********"
-cp -f java-app/target/*.jar /home/ubuntu/master_jenkins/jenkins/pipeline
+#cp -f java-app/target/*.jar /home/ubuntu/master_jenkins/jenkins/pipeline
+cp -f java-app/target/*.jar .
 sleep 3
 
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd /home/ubuntu/master_jenkins/jenkins/pipeline && docker-compose -f docker-compose-build.yml build --no-cache
+#cd /home/ubuntu/master_jenkins/jenkins/pipeline && docker-compose -f docker-compose-build.yml build --no-cache
+docker-compose -f docker-compose-build.yml build --no-cache
