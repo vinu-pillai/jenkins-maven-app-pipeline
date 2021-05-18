9940138ec00bbd2d6ce01ce3cbd9e4eefa01dfea
Ubuntu <ubuntu@ip-172-31-11-55.ap-south-1.compute.internal>
2021-05-18 13:15:37 +0000

    Workspace variable updated in mvn.sh

--- mvn.sh
+++ mvn.sh
@@ -4,6 +4,6 @@ echo "******************************************"
 echo "******* Building The jar - By Vinu! ******"
 echo "******************************************"
 
-WORKSPACE=/home/ubuntu/master_jenkins/jenkins/pipeline
+WORKSPACE=/master_jenkins/jenkins/jenkins_home/workspace/jenkins-maven-app-project
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.1-openjdk-17-slim "$@"
